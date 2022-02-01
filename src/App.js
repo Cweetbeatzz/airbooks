7e2bf604bd338e2c456334da7ea78ea8eaacf615
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-02-01 16:44:32 +0100

    added features2

--- src/App.js
+++ src/App.js
@@ -2,6 +2,20 @@ import "./App.css";
 import Footer from "./components/footer/Footer.js";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
+import Products from "./components/products/Products";
+import CreateProducts from "./components/products/Create";
+import UpdateProducts from "./components/products/Update";
+import DeleteProducts from "./components/products/Delete";
+import Categories from "./components/categories/Categories";
+import CreateCategories from "./components/categories/Create";
+import UpdateCategories from "./components/categories/Update";
+import DeleteCategories from "./components/categories/Delete";
+import Cart from "./components/cart/Cart";
+import ProdCategories from "./components/prodByCategories/ProdByCategories";
+import Register from "./components/register/Register";
+import Login from "./components/login/Login";
+import Dashboard from "./components/dashboard/Dashboard";
+
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -14,6 +28,20 @@ function App() {
         <Routes>
           <Route path="*" element={<Home />} />
           <Route path="home" element={<Home />} />
+          <Route path="Products" element={<Products />} />
+          <Route path="CreateProducts" element={<CreateProducts />} />
+          <Route path="UpdateProducts" element={<UpdateProducts />} />
+          <Route path="DeleteProducts" element={<DeleteProducts />} />
+          <Route path="Categories" element={<Categories />} />
+          <Route path="CreateCategories" element={<CreateCategories />} />
+          <Route path="UpdateCategories" element={<UpdateCategories />} />
+          <Route path="DeleteCategories" element={<DeleteCategories />} />
+          <Route path="DeleteCategories" element={<DeleteCategories />} />
+          <Route path="Cart" element={<Cart />} />
+          <Route path="ProdCategories" element={<ProdCategories />} />
+          <Route path="Register" element={<Register />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Dashboard" element={<Dashboard />} />
         </Routes>
 
         <Footer />
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,41 +1,113 @@
 import React, { useState } from "react";
 import { ReactDOM } from "react";
-import {
-  Container,
-  Menu,
-  RightMenu,
-  CustomMenu,
-  BurgerNav,
-} from "./Header.styles";
+import { Container } from "./Header.styles";
 import { Link } from "react-router-dom";
-
-// import MenuIcon from '@material-ui/icons/Menu';
-// import CloseIcon from '@material-ui/icons/Close';
 
 function Header(params) {
   return (
-    <Container>
-      <Link to="Index">AIRBOOKS</Link>
-      <Menu>
-        <Link to="home">home</Link>
-        <Link to="reservation">reservation</Link>
-        <Link to="contact">contact</Link>
-        <Link to="about">about</Link>
-        <Link to="privacy">privacy</Link>
-      </Menu>
-      <RightMenu>
-        <Link to="home">bookings</Link>
-        <CustomMenu />
-      </RightMenu>
-      {/* <BurgerNav>
-                    <li><a href="home">country</a></li>
-                    <li><a href="home">depatures</a></li>
-                    <li><a href="home">arrivals</a></li>
-                    <li><a href="home">price feeds</a></li>
-                    <li><a href="home">schedules</a></li>
-                    <li><a href="home">location</a></li>
-               </BurgerNav> */}
-    </Container>
+    <header>
+      <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light bg-light border-bottom box-shadow">
+        <div class="container">
+          <Link class="navbar-brand" to="home">
+            <img src="images/fashion5.png" alt="" />
+          </Link>
+          <button
+            class="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target=".navbar-collapse"
+            aria-controls="navbarSupportedContent"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
+            <ul class="navbar-nav flex-grow-1">
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="products">
+                  <strong>STORE</strong>
+                </Link>
+              </li>
+
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="login">
+                  <strong>LOGIN</strong>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="register">
+                  <strong>REGISTER</strong>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="contact">
+                  <strong>CONTACT</strong>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="about">
+                  <strong>ABOUT US</strong>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="privacy">
+                  <strong>PRIVACY</strong>
+                </Link>
+              </li>
+              <li class="nav-item">
+                <Link class="nav-link text-dark" to="admin">
+                  <strong>ADMIN</strong>
+                </Link>
+              </li>
+            </ul>
+            <Link class="btn btn-default btn-sm  m-0" to="cart">
+              CART{" "}
+              <span
+                class="text-white bg-danger badge rounded-pill "
+                id="cartquantity"
+              >
+                0
+              </span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg "
+                width="18"
+                height="18"
+                fill="currentColor "
+                class="bi bi-cart3 "
+                viewBox="0 0 16 16 "
+              >
+                <path
+                  d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .49.598l-1 5a.5.5 0 0 1-.465.401l-9.397.472L4.415 11H13a.5.5 0 0 1 0 1H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5zM3.102 4l.84
+                           4.479 9.144-.459L13.89 4H3.102zM5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4zm-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2zm7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2z "
+                />
+              </svg>
+            </Link>
+            <Link class="btn btn-default btn-sm m-0" to="account">
+              {" "}
+              ACCOUNT{" "}
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="16"
+                height="16"
+                fill="currentColor"
+                class="bi bi-person-circle"
+                viewBox="0 0 16 16"
+              >
+                <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
+                <path
+                  fill-rule="evenodd"
+                  d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"
+                />
+              </svg>
+            </Link>
+            <a class="nav-link text-dark" href="link" to="logout">
+              <strong>LOGOUT</strong>
+            </a>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
 
