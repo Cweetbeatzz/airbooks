e5356ca69bec7c0cd220cfd137cc3200db1cf992
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-02-02 08:55:45 +0100

    updates

--- Backend/Server.js
+++ Backend/Server.js
@@ -2,11 +2,17 @@ import express from "express";
 
 const app = express();
 
-//
+//#########################################################
+
 app.get("/", (req, res) => {
   res.send("Server is Running");
 });
+//#########################################################
+
+const port = process.env.PORT || 5000;
+
+//#########################################################
 
 app.listen(5000, () => {
-  console.log("Server Running on http://localhost:5000");
+  console.log(`Server Running on ${port}`);
 });
