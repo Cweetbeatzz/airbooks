39c9f21a3dbdcf86e5dd56422a6c54c13c5dfdb8
Cweetbeatzz <Cweetbeatz@gmail.com>
2022-11-13 11:01:57 +0100

    updates on graphql

---
+++ BackendGraphQl/Fashion5/app.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+require("dotenv").config();
+const usersRouter = require("./Controllers/UsersController");
+const categoryRouter = require("./Controllers/CategoryController");
+const productsRouter = require("./Controllers/ProductsController");
+const authRouter = require("./Controllers/Auth");
+const stripeRouter = require("./Controllers/StripeController");
+const db = require("./Database/connection");
+require("express-async-errors");
+const cors = require("cors");
+const socket = require("socket.io");
+const http = require("http");
+const app = express();
+
+//<----------- middlewares ---------------->
+//use static files
+app.use("/uploads", express.static("./uploads"));
+app.use(express.json());
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+
+//
+app.get("/", (req, res) => {
+  res.send(`<h2>Welcome to Fashion5 API</h3>`);
+});
+//#############################################################
+let server;
+server = http.createServer(app);
+//#############################################################
+
+//<----------- routes ---------------->
+app.use("/fashion5/api/v1/products", productsRouter);
+app.use("/fashion5/api/v1/users", usersRouter);
+app.use("/fashion5/api/v1/category", categoryRouter);
+app.use("/fashion5/api/v1/auth", authRouter);
+app.use("/fashion5/api/v1/stripe", stripeRouter);
+
+const port = process.env.PORT || 6800;
+
+//#############################################################
+
+//database check
+const startDB = async () => {
+  try {
+    await db(
+      process.env.MONGO_DB_CONNECTION_STRING ||
+        "mongodb://localhost:27017/Fashion5"
+    );
+    //server connection
+    server = app.listen(
+      port,
+      console.log(`Server is running on http://localhost:${port}`)
+    );
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+//#############################################################
+
+module.export = server;
+startDB();
