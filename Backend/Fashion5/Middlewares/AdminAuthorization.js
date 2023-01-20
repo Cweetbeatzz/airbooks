2c790a311e8dd109543bc9ca61535cfb17177507
Cweetbeatzz <Cweetbeatz@gmail.com>
2023-01-20 14:57:43 +0100

    updates

--- Backend/Fashion5/Middlewares/AdminAuthorization.js
+++ Backend/Fashion5/Middlewares/AdminAuthorization.js
@@ -1,12 +1,20 @@
 const { roles } = require("../utils/constants");
+const { verifyToken } = require("../Services/Jwt");
 
 //#######################################################
 
 const ensureAdmin = (req, res, next) => {
-    const authorization = req.headers.authorization;
-  if (req.User.role2 === roles.admin) {
-    next();
-  } else {
+  const authorization = req.headers.authorization;
+
+  try {
+    const token = authorization.split("")[1];
+    const decoded = verifyToken(token);
+    const { role2 } = decoded;
+
+    if (role2 === roles.admin) {
+      next();
+    }
+  } catch (error) {
     res
       .status(401)
       .send({ message: "Error", data: "Not Authorized...Admin Access only" });
